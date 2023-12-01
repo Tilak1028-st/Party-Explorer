ade17db603b4c2d46b9124562edad35865bfe45a
Shakya Tilak <shakyatilak45@gmail.com>
2023-12-01 11:52:24 +0530

    Initial Commit

---
+++ Party Explorer/Party_ExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Party_ExplorerApp.swift
+//  Party Explorer
+//
+//  Created by Tilak Shakya on 01/12/23.
+//
+
+import SwiftUI
+
+@main
+struct Party_ExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
